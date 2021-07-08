13dd15e9a46d688bfded17070a367a2c5da67bfc
Iliana00 <carrasquedo.279@gmail.com>
2021-07-08 18:57:59 -0500

    validator.js

--- src/validator.js
+++ src/validator.js
@@ -1,5 +1,36 @@
 const validator = {
-  // ...
-};
-
+  isValid : function(creditCardNumber) {
+    const numero = Array.from(creditCardNumber).map(Number).reverse();
+    let suma = 0;
+    for (let i = 0; i < numero.length; i++){
+      if (i % 2 === 1){
+        let mult = numero[i] * 2;
+        if (mult >= 10){
+        mult = mult - 10 + 1;
+        }
+        suma = suma + mult;
+      } else {
+        suma = suma + numero[i];
+      }      
+    } 
+    if (suma % 10 === 0){
+      return true
+    } else{
+      return false
+    }       
+ },
+ maskify : function(creditCardNumber){
+   const mask = creditCardNumber.length
+  if (mask <= 4){
+    return creditCardNumber
+  } else {
+    let listado = Array.from(creditCardNumber);
+    for(let i = 0; i < creditCardNumber.length-4; i++){
+      listado[i] ="#"
+    }
+    let enmascarar = listado.join("")
+    return enmascarar
+  }
+ }
+}
 export default validator;
